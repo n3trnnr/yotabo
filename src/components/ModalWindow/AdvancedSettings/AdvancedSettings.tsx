0302df09633614e8b21a45c9b88c98cd9dad3a98
n3trnnr <n3trnnr@gmail.com>
2024-04-07 21:31:57 +0300

    Рефакторинг компонента AdvancedSetting

--- src/components/ModalWindow/AdvancedSettings/AdvancedSettings.tsx
+++ src/components/ModalWindow/AdvancedSettings/AdvancedSettings.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import styles from './AdvancedSettings.module.scss'
 import SvgIcons from "../../UI/Svg/SvgIcons";
 import { nanoid } from "nanoid";
+import Button from "../../UI/Button/Button";
 
 //Доделать hover окружности radio!!!
 
@@ -18,14 +19,13 @@ const AdvancedSettings: React.FC = () => {
 
     const deadline = (event: any) => {
         console.log(event.target.value);
-
     }
-
 
     return (
         <div className={styles["advanced-settings-wrapper"]}>
             <div className={styles["info-container"]}>
                 <div className={styles["info-wrapper"]}>
+
                     <div className={styles["priority-wrapper"]}>
                         <div className={styles["priority-title"]}>Priority</div>
                         <div className={styles["radios-wrapper"]}>
@@ -34,10 +34,10 @@ const AdvancedSettings: React.FC = () => {
                                     className={styles["input-radio"]}
                                     type="radio"
                                     name="priority-radio"
-                                    id="1" defaultValue={"low"}
+                                    defaultValue={"low"}
                                     onClick={(event) => radio(event)}
                                 />
-                                <span>Low</span>
+                                <span className={styles["radio-description"]}>Low</span>
                             </label>
 
                             <label className={styles["radio"]}>
@@ -45,10 +45,9 @@ const AdvancedSettings: React.FC = () => {
                                     className={styles["input-radio"]}
                                     type="radio"
                                     name="priority-radio"
-                                    id="2"
                                     defaultValue={"med"}
                                     onClick={(event) => radio(event)} />
-                                <span>Med</span>
+                                <span className={styles["radio-description"]}>Med</span>
                             </label>
 
                             <label className={styles["radio"]}>
@@ -56,10 +55,9 @@ const AdvancedSettings: React.FC = () => {
                                     className={styles["input-radio"]}
                                     type="radio"
                                     name="priority-radio"
-                                    id="3"
                                     defaultValue={"high"}
                                     onClick={(event) => radio(event)} />
-                                <span>High</span>
+                                <span className={styles["radio-description"]}>High</span>
                             </label>
                         </div>
                     </div>
@@ -73,11 +71,13 @@ const AdvancedSettings: React.FC = () => {
                             />
                         </div>
                     </div>
+
                 </div>
             </div>
 
             <div className={styles["files-container"]}>
                 <div className={styles["files-wrapper"]}>
+
                     <div className={styles["cover-wrapper"]}>
                         <label className={styles["upload-cover-wrapper"]}>
                             <SvgIcons
@@ -87,7 +87,22 @@ const AdvancedSettings: React.FC = () => {
                             Upload cover
                             <input type="file" onChange={(event) => setCover(event.target.value)} />
                         </label>
-                        <div className={styles["cover-value"]}>{cover ? cover : 'cover'}</div>
+                        <ul className={styles["cover-value"]}>
+                            {cover ?
+                                <li className={styles["file-item"]}>
+                                    <span className={styles["file-value"]}>
+                                        {cover}
+                                    </span>
+                                    <Button colorStyle={"none"}>
+                                        <SvgIcons
+                                            iconName={"trash"}
+                                            styleName={styles["delete-icon-button"]}
+                                        />
+                                    </Button>
+                                </li> :
+                                '-'
+                            }
+                        </ul>
                     </div>
 
                     <div className={styles["file-wrapper"]}>
@@ -99,14 +114,25 @@ const AdvancedSettings: React.FC = () => {
                             Upload file
                             <input type="file" onChange={(event) => setFiles([...files, event.target.value])} />
                         </label>
-                        <div className={styles["file-value"]}>
+                        <ul>
                             {files.length ? files.map((file: string) => (
-                                <>{file}</>
+                                <li className={styles["file-item"]}>
+                                    <span className={styles["file-value"]}>
+                                        {file}
+                                    </span>
+                                    <Button colorStyle={"none"}>
+                                        <SvgIcons
+                                            iconName={"trash"}
+                                            styleName={styles["delete-icon-button"]}
+                                        />
+                                    </Button>
+                                </li>
                             ))
-                                : "files"
+                                : <li className={styles["file-value"]}>-</li>
                             }
-                        </div>
+                        </ul>
                     </div>
+
                 </div>
             </div>
         </div>
